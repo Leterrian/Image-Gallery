d74fb925df914bea6440cbd90cd73bd6271459c6
Leterrian Israel <ljisrael@coastal.edu>
2025-01-30 21:46:10 -0500

    debugging.

--- main.js
+++ main.js
@@ -5,27 +5,41 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-const filenames = ["pic1.jpg", "pic2.jpg", "pic3.jpg" , "pic4.jpg", "pic5.jpg"];
 
-/* Declaring the alternative text for each image file */
-const alt = {
-    "pic1.jpg" : "Close up of eye",
-    "pic2.jpg" : "ocean looking rock",
-    "pic3.jpg" : "white and purple flowers",
-    "pic4.jpg" : "pharoah painting",
-    "pic5.jpg" : "moth on a leaf"
-
-};
-
-
-
+const images = ['pic1.jpg', `pic2.jpg`, `pic3.jpg`, `pic4.jpg`, `pic5.jpg`];
+const alts = {
+  'pic1.jpg' : 'Closeup of a human eye',
+  'pic2.jpg' : 'Rock that looks like a wave',
+  'pic3.jpg' : 'Purple and white pansies',
+  'pic4.jpg' : 'Section of wall from a pharoah\'s tomb',
+  'pic5.jpg' : 'Large moth on a leaf'
+}
 
 /* Looping through images */
-for (const files in filenames)  {
-    const newImage = document.createElement('img');
-    newImage.setAttribute('src', `images/${files}`);
-    newImage.setAttribute('alt', alt[image]);
-    thumbBar.appendChild(newImage);
-    
-    } 
+
+for (const image of images) {
+  const newImage = document.createElement('img');
+  newImage.setAttribute('src', `images/${image}`);
+  newImage.setAttribute('alt', alts[image]);
+  thumbBar.appendChild(newImage);
+  newImage.addEventListener('click', e => {
+    displayedImage.src = e.target.src;
+    displayedImage.alt = e.target.alt;
+  });
+}
+
 /* Wiring up the Darken/Lighten button */
+
+btn.addEventListener('click', () => {
+  const btnClass = btn.getAttribute('class');
+  if (btnClass === 'dark') {
+    btn.setAttribute('class','light');
+    btn.textContent = 'Lighten';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+  } else {
+    btn.setAttribute('class','dark');
+    btn.textContent = 'Darken';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+  }
+});
+
